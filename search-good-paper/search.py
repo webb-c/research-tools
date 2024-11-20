7c53a9ca6f869280b28b5919ccf7011606184da4
webb-c <jwst0208@gmail.com>
2024-11-20 17:25:26 +0900

    add: search code

---
+++ search-good-paper/search.py
@@ -0,0 +1,22 @@
+from semanticscholar import SemanticScholar
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--id", type=str, default=None)
+    parser.add_argument("--query", type=str, default=None)
+    parser.add_argument("--limit", type=int, default=10)
+    
+    args = parser.parse_args()
+
+    sch = SemanticScholar()
+    
+    if args.query is not None:
+        results = sch.search_paper(args.query, limit=args.limit)
+        for item in results.items:
+            print(item.title)
+            
+    elif args.id is not None:
+        results = sch.get_recommended_papers(args.id, limit=args.limit)
+        for item in results:
+            print(item.title)
